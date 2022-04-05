5d3e9efc1031ced8c08d3c6862da4bafe7556496
Alex Volinsky <alex.volinsky@gmail.com>
2022-04-05 14:27:04 -0400

    Added support for CONTROL flags in get/set xattr (#1)

--- source3/libsmb/libsmb_xattr.c
+++ source3/libsmb/libsmb_xattr.c
@@ -438,11 +438,17 @@ sec_desc_parse(TALLOC_CTX *ctx,
 	bool have_owner = false, have_group = false;
 	struct security_acl *dacl=NULL;
 	int revision=1;
+	uint16_t type=SEC_DESC_SELF_RELATIVE;
 
 	while (next_token_talloc(ctx, &p, &tok, "\t,\r\n")) {
 
 		if (strncasecmp_m(tok,"REVISION:", 9) == 0) {
 			revision = strtol(tok+9, NULL, 16);
+			continue;
+		}
+
+		if (strncasecmp_m(tok,"CONTROL:", 8) == 0) {
+			type = strtol(tok+8, NULL, 0);
 			continue;
 		}
 
@@ -539,7 +545,7 @@ sec_desc_parse(TALLOC_CTX *ctx,
 	ret = make_sec_desc(
 		ctx,
 		revision,
-		SEC_DESC_SELF_RELATIVE,
+		type,
 		have_owner ? &owner_sid : NULL,
 		have_group ? &group_sid : NULL,
 		NULL,
@@ -951,6 +957,30 @@ cacl_get(SMBCCTX *context,
                                         n = snprintf(buf, bufsize, "%d",
                                                      sd->revision);
                                 }
+                        }
+
+                        if (!determine_size && n > bufsize) {
+                                errno = ERANGE;
+                                return -1;
+                        }
+                        buf += n;
+                        n_used += n;
+                        bufsize -= n;
+                        n = 0;
+                }
+
+                if (all || all_nt) {
+                        if (determine_size) {
+                                p = talloc_asprintf(ctx, ",CONTROL:0x%x",
+                                                         sd->type);
+                                if (!p) {
+                                        errno = ENOMEM;
+                                        return -1;
+                                }
+                                n = strlen(p);
+                        } else if (sidstr[0] != '\0') {
+                                n = snprintf(buf, bufsize,
+                                                  ",CONTROL:0x%x", sd->type);
                         }
 
                         if (!determine_size && n > bufsize) {
@@ -1693,7 +1723,7 @@ cacl_set(SMBCCTX *context,
 	sort_acl(old->dacl);
 
 	/* Create new security descriptor and set it */
-	sd = make_sec_desc(ctx, old->revision, SEC_DESC_SELF_RELATIVE,
+	sd = make_sec_desc(ctx, old->revision, sd->type,
 			   owner_sid, group_sid, NULL, dacl, &sd_size);
 
 	status = cli_ntcreate(targetcli, targetpath, 0,
