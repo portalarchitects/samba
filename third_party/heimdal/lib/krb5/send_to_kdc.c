ec16ac6f46525b626a399378f26726f89be7ff53
Jim Partin <jpartin@skysync.com>
2024-12-07 10:45:51 -0500

    Fix poll logic. Rework poll changes to not use heim dictionary.

--- third_party/heimdal/lib/krb5/send_to_kdc.c
+++ third_party/heimdal/lib/krb5/send_to_kdc.c
@@ -973,8 +973,11 @@ struct wait_ctx {
     krb5_sendto_ctx ctx;
     // fd_set rfds;
     // fd_set wfds;
+
+	struct pollfd *fds;
+	int nfds;
+
     rk_socket_t max_fd;
-	heim_dict_t fds;
     int got_reply;
     time_t timenow;
 };
@@ -1012,27 +1015,32 @@ wait_setup(heim_object_t obj, void *iter_ctx, int *stop)
 // #ifndef NO_LIMIT_FD_SETSIZE
 //     heim_assert(h->fd < FD_SETSIZE, "fd too large");
 // #endif
-	heim_object_t k = heim_number_create(h->fd);
-	heim_object_t v;
+
+	short flags = 0;
 
     switch (h->state) {
     case WAITING_REPLY:
 	//FD_SET(h->fd, &wait_ctx->rfds);
-	heim_dict_set_value(wait_ctx->fds, k, v = heim_number_create(POLLIN));
-	heim_release(v);
+	flags = POLLIN;
 	break;
     case CONNECTING:
     case CONNECTED:
 	// FD_SET(h->fd, &wait_ctx->rfds);
 	// FD_SET(h->fd, &wait_ctx->wfds);
-	heim_dict_set_value(wait_ctx->fds, k, v = heim_number_create(POLLIN | POLLOUT));
-	heim_release(v);
+	flags = POLLIN | POLLOUT;
 	break;
     default:
 	debug_host(wait_ctx->context, 5, h, "invalid sendto host state");
 	heim_abort("invalid sendto host state");
     }
-	heim_release(k);
+
+	if (flags != 0) {
+		wait_ctx->fds[wait_ctx->nfds].fd = h->fd;
+		wait_ctx->fds[wait_ctx->nfds].events = flags;
+		wait_ctx->fds[wait_ctx->nfds].revents = 0;
+		wait_ctx->nfds++;
+	}
+
     if (h->fd > wait_ctx->max_fd || wait_ctx->max_fd == rk_INVALID_SOCKET)
 	wait_ctx->max_fd = h->fd;
 }
@@ -1068,18 +1076,23 @@ wait_process(heim_object_t obj, void *ctx, int *stop)
 // #endif
     // readable = FD_ISSET(h->fd, &wait_ctx->rfds);
     // writeable = FD_ISSET(h->fd, &wait_ctx->wfds);
-	k = heim_number_create(h->fd);
-	v = heim_dict_get_value(wait_ctx->fds, k);
-	if (v) {
-		flags = heim_number_get_int(v);
+
+	int fd_idx = -1;
+	for (int i = 0; i < wait_ctx->nfds; i++) {
+		if (wait_ctx->fds[i].fd == h->fd) {
+			fd_idx = i;
+			break;
+		}
+	}
+
+	if (fd_idx > -1) {
+		short flags = wait_ctx->fds[fd_idx].revents;
 		readable = flags & POLLIN;
 		writeable = flags & POLLOUT;
-		heim_release(v);
 	} else {
 		readable = 0;
 		writeable = 0;
 	}
-	heim_release(k);
 
     if (readable || writeable || h->state == CONNECT)
 	wait_ctx->got_reply |= eval_host_state(wait_ctx->context, wait_ctx->ctx, h, readable, writeable);
@@ -1087,24 +1100,6 @@ wait_process(heim_object_t obj, void *ctx, int *stop)
     /* if there is already a reply, just fall though the array */
     if (wait_ctx->got_reply)
 	*stop = 1;
-}
-
-static void
-count_fds(heim_object_t k, heim_object_t v, void *arg)
-{
-	(*((int *) arg))++;
-}
-
-static void
-build_poll_request(heim_object_t k, heim_object_t v, void *arg)
-{
-    struct pollfd **ppollfds = arg;
-
-	(*ppollfds)->fd = heim_number_get_int(k);
-	(*ppollfds)->events = heim_number_get_int(v);
-	(*ppollfds)->revents = 0;
-
-	(*ppollfds)++;
 }
 
 static krb5_error_code
@@ -1119,12 +1114,14 @@ wait_response(krb5_context context, int *action, krb5_sendto_ctx ctx)
     // FD_ZERO(&wait_ctx.rfds);
     // FD_ZERO(&wait_ctx.wfds);
     wait_ctx.max_fd = rk_INVALID_SOCKET;
-	wait_ctx.fds = heim_dict_create(10);
+	
+	struct pollfd fds[heim_array_get_length(ctx->hosts)];
+	wait_ctx.fds = &fds;
+	wait_ctx.nfds = 0;
 
     /* oh, we have a reply, it must be a plugin that got it for us */
     if (ctx->response.length) {
 	*action = KRB5_SENDTO_FILTER;
-	heim_release(wait_ctx.fds);
 	return 0;
     }
 
@@ -1143,7 +1140,6 @@ wait_response(krb5_context context, int *action, krb5_sendto_ctx ctx)
 			 "and no more hosts -> failure");
 	    *action = KRB5_SENDTO_TIMEOUT;
 	}
-	heim_release(wait_ctx.fds);
 	return 0;
     }
 
@@ -1155,7 +1151,6 @@ wait_response(krb5_context context, int *action, krb5_sendto_ctx ctx)
 	 */
 	_krb5_debug(context, 5, "wait_response: moving the contestants forward");
 	heim_array_iterate_f(ctx->hosts, &wait_ctx, wait_accelerate);
-	heim_release(wait_ctx.fds);
 	return 0;
     }
 
@@ -1163,19 +1158,12 @@ wait_response(krb5_context context, int *action, krb5_sendto_ctx ctx)
     tv.tv_usec = 0;
 
     //ret = select(wait_ctx.max_fd + 1, &wait_ctx.rfds, &wait_ctx.wfds, NULL, &tv);
-	int fd_count = 0;
-	heim_dict_iterate_f(wait_ctx.fds, &fd_count, count_fds);
 
-	struct pollfd fds[fd_count + 1];
-	struct pollfd *currfd = &fds[0];
-	heim_dict_iterate_f(wait_ctx.fds, &currfd, build_poll_request);
-
-	ret = poll(fds, fd_count, tv.tv_sec * 1000 + tv.tv_usec / 1000);
+	ret = poll(wait_ctx.fds, wait_ctx.nfds, tv.tv_sec * 1000 + tv.tv_usec / 1000);
     if (ret < 0)
 	return errno;
     if (ret == 0) {
 	*action = KRB5_SENDTO_TIMEOUT;
-	heim_release(wait_ctx.fds);
 	return 0;
     }
 
@@ -1185,8 +1173,6 @@ wait_response(krb5_context context, int *action, krb5_sendto_ctx ctx)
 	*action = KRB5_SENDTO_FILTER;
     else
 	*action = KRB5_SENDTO_CONTINUE;
-
-	heim_release(wait_ctx.fds);
 
     return 0;
 }
